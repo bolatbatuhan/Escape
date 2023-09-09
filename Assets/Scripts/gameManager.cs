70934d4e1e899a1bf27f6f247fd635f00a1d1a15
Batuhan Bolat <bolatbatuhan@gmail.com>
2023-09-09 12:58:18 +0300

    updated score and coin collect

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    public Text scoreText;
+    int score = 0;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        score = 0;
+        updateScoreText();
+        
+    }
+
+    public void addScore(int amount)
+    {
+        score += amount;
+        updateScoreText();
+    }
+
+    void updateScoreText()
+    {
+        scoreText.text = "x " + score.ToString();
+    }
+
+}
