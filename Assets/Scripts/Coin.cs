1b1b6179acf5c173dbbb7baafeb6c28bf5d61162
Batuhan Bolat <bolatbatuhan@gmail.com>
2023-09-09 13:55:55 +0300

    Fixed coin collision

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,13 +6,10 @@ public class Coin : MonoBehaviour
 {
     public int coinValue = 10;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            gameManager.instance.addScore(coinValue);
-            Destroy(gameObject);
-        }
+        gameManager.instance.addScore(coinValue);
+        Destroy(gameObject);
     }
 
 }
