815383c0643c4a1dae1a2a22c2348f2cb9e7a03e
Batuhan Bolat <bolatbatuhan@gmail.com>
2023-09-22 12:23:09 +0300

    added bullet lifetime, enemy health, bullet enemy collision,enemy getdmg

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    public float bulletDamage, lifeTime;
+    
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public float health = 100.0f;
+    private void Start() {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("bullet"))
+        {
+            GetDamage(collision.gameObject.GetComponent<BulletManager>().bulletDamage);
+        }
+    }
+    public void GetDamage(float damage)
+    {
+        if ((health - damage) >= 0)
+        {
+            health -= damage;
+        }
+        else
+        {
+            health = 0;
+        }
+        AmIDead();
+
+    }
+
+    void AmIDead()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
