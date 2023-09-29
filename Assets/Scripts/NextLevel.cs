0f0147369dd25678356ada7bbbd46f26eba87bee
Batuhan Bolat <bolatbatuhan@gmail.com>
2023-09-29 11:48:50 +0300

    updated Environments and added LevelManager , EndTrigger

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+
+private void OnTriggerEnter2D(Collider2D other) {
+    LoadNextLevel();
+}
+
+   public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   
+    }
+
+}
